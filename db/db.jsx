8ab6ca9445f94ba70546068fb769ac9256747daa
Miguel Páez <mpaez@ikatechsolutions.com>
2024-07-20 23:44:31 -0500

    Update changes

--- db/db.jsx
+++ db/db.jsx
@@ -1,7 +1,7 @@
 //el envío de datos de El registro de usuarios
 
 export async function enviarDatos(dataObject) {
-    const response = await fetch('https://sistemas-backend-analitica-django-mongo.onrender.com/insertar_usuario/', {
+    const response = await fetch('https://backend-sistemas-autoorganizados-2.onrender.com/usersIntoPage', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -13,16 +13,3 @@ export async function enviarDatos(dataObject) {
     return data;
   }
 
-
-  export async function enviarDatosTiempo(dataObjectTiempo) {
-    const response = await fetch('https://sistemas-backend-analitica-django-mongo.onrender.com/insertar_usuario/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(dataObjectTiempo),
-    });
-  
-    const data = await response.json();
-    return data;
-  }
--- src/components/usuarios_time.jsx
+++ src/components/usuarios_time.jsx
@@ -1,249 +1,130 @@
-import {  useEffect, useState } from 'react';
-import { enviarDatos,  enviarDatosTiempo} from '../../db/db.jsx';
+import {  useEffect, useState, useRef } from 'react';
+import { enviarDatos} from '../../db/db.jsx';
 
 const Usuario = () => {
   //-----Variables y constantes---------
-  const [usuario, setNombreUsuario] = useState('');
-  const [fecha_ingreso, setFechaIngreso] = useState('');
-  const [hora_ingreso, setHoraIngreso] = useState('');
-  const [miIP, setDataIPS] = useState('');
-  const [miPais, setMiPais] = useState('');
-  const [miCiudad, setMiCiudad] = useState('');
-  const [ruta, setRuta] = useState('');
-  const [tiempo, setTiempoPermanencia] = useState('00:00:00');
-  const [tiempoAleatorio, setTiempoAleatorio] = useState('00:00:00');
-  const [tiempoInicio] = useState(new Date());
-  const [dispositivo, setDispositivo] = useState('');
+  const [miIP, setDataIPS] = useState('')
+  const [miPais, setMiPais] = useState('')
+  const [miPaisAleatorio, setMiPaisAleatorio] = useState('')
+  const [miCiudad, setMiCiudad] = useState('')
+  const [miCiudadAleatorio, setMiCiudadAleatorio] = useState('')
+  const [ruta, setRuta] = useState('')
+  const [dispositivo, setDispositivo] = useState('')
+  const hasRun = useRef(false)
 
-  const ciudades = [
-    "Leticia", "Medellín", "Arauca", "Barranquilla", "Cartagena", "Tunja",
-    "Manizales", "Florencia", "Yopal", "Popayán", "Valledupar", "Quibdó",
-    "Montería", "Bogotá", "Inírida", "San José del Guaviare", "Neiva",
-    "Riohacha", "Santa Marta", "Villavicencio", "Pasto", "Cúcuta", "Mocoa",
-    "Armenia", "Pereira", "San Andrés", "Bucaramanga", "Sincelejo", "Ibagué",
-    "Cali", "Mitú", "Puerto Carreño"
-]
+  const paises = [
+    'Argentina', 'Bolivia', 'Chile', 'Colombia', 'Costa Rica', 'Cuba',
+    'Ecuador', 'El Salvador', 'Guatemala', 'Honduras', 'México',
+    'Nicaragua', 'Panamá', 'Paraguay', 'Perú', 'República Dominicana',
+    'Uruguay', 'Venezuela'
+  ];
+  
+  const ciudadesPorPais = {
+    Argentina: ["Buenos Aires", "Catamarca", "Chaco", "Chubut", "Cordoba", "Corrientes", "Entre Ríos", "Formosa", "Jujuy", "La Pampa", "La Rioja", "Mendoza", "Misiones", "Neuquen", "Rio Negro", "Salta", "San Juan", "San Luis", "Santa Cruz", "Santa Fe", "Santiago del Estero", "Tierra del Fuego", "Tucumán"],
+    Bolivia: ["La Paz", "Cochabamba", "Santa Cruz", "Tarija"],
+    Chile: ["Santiago", "Valparaíso", "Arica"],
+    Colombia: ["Barranquilla", "Bogotá", "Cali", "Cartagena", "Medellín", "Santa Marta", "Villavicencio", "Pasto", "Cucuta", "Manizales", "Neiva", "Soledad", "Armenia", "Buenaventura", "Calarca", "Ibague", "Manizales", "Popayan", "Tunja", "Valledupar", "Monteria", "Pereira", "Quibdo", "Riohacha", "Santa Marta", "Sincelejo", "Tumaco", "Villavicencio", "Cucuta", "Pasto", "Tunja", "Manizales", "Monteria", "Pereira", "Popayãn", "Quibdo", "Tumaco", "Villavicencio", "Valledupar", "Monteria", "Pereira", "Popayán", "Quibdo"],
+    CostaRica: ["San Jose", "Alajuela", "Cartago"],
+    Cuba: ["Havana", "La Habana", "Pinar del Rio"],
+    Ecuador: ["Guayaquil", "Quito", "Cuenca", "Loja"],
+    ElSalvador: ["San Salvador", "Santa Ana", "San Vicente"],
+    Guatemala: ["Guatemala", "Alta Verapaz", "Baja Verapaz"],
+    Honduras: ["Tegucigalpa", "San Pedro Sula", "La Ceiba"],
+    Mexico: ["Ciudad de Máxico", "Gustavo A. Madero", "Iztapalapa"],
+    Nicaragua: ["Managua", "Chinandega", "Jinotega"],
+    Panama: ["Panamá", "Colón", "Chiriquí"],
+    Paraguay: ["Asunción", "Ciudad del Este", "Paraguana"],
+    Peru: ["Lima", "Callao", "Trujillo"],
+    RepublicaDominicana: ["Santo Domingo", "Santiago", "San Cristóbal"],
+    Uruguay: ["Montevideo", "Colón", "Canelones"],
+    Venezuela: ["Caracas", "Maracaibo", "Carabobo"]
+  };
+  
+  const paisAleatorioFn = () => {
+    console.log("Entro")
+    const random = Math.floor(Math.random() * 100);
+    let paisAleatorio
 
-
-
-  const ciudadfuntion = () => {
-    if (miCiudad == "Pereira" || miCiudad == "Ciudad no registrada"){
-      let randomNumber = Math.floor(Math.random() * ciudades.length); 
-      let selectedCity = ciudades[randomNumber]; 
-      setMiCiudad(selectedCity);
+    if (random >= 0 && random <= 50) {
+      paisAleatorio = 'Colombia'
+    } else {
+      const index = Math.floor(Math.random() * (paises.length - 1))
+      paisAleatorio = paises.filter(pais => pais !== 'Colombia')[index]
     }
-  }
+
+    setMiPaisAleatorio(paisAleatorio)
+
+    // Seleccionar una ciudad aleatoria según el país seleccionado
+    const ciudades = ciudadesPorPais[paisAleatorio]
+    const ciudadAleatoria = ciudades[Math.floor(Math.random() * ciudades.length)]
+    setMiCiudadAleatorio(ciudadAleatoria)
+  };
+
+  useEffect(() => {
+    paisAleatorioFn();
+  }, [])
 
 
   //-----Variables y constantes---------
-  const objetoDataUsuarioTiempo = {
-    "usuario": usuario,
-    "fecha_ingreso": fecha_ingreso,
+  const objetoData = {
     "pais": miPais,
-    "hora_ingreso": hora_ingreso,
     "ciudad": miCiudad,
     "ruta": ruta,
-    "tiempo": tiempoAleatorio,
     "dispositivo": dispositivo
-  };
-
-  const enviarDatosfunctionTiempo = () => {
-    console.log("Soy el objeto externo por 3 segundos",objetoDataUsuarioTiempo);
-    enviarDatosTiempo(objetoDataUsuarioTiempo)
-
   }
 
-  if(tiempo == '00:00:03'){
-    ciudadfuntion();
-    enviarDatosfunctionTiempo();
-  }
 
-  
-
-  useEffect(() => {
-
-    // ----
-    // const [varMiIP, setVarMiIP] = useState('');
-
-
-    // ----------Función Usuario aleatorio---------------
-    //  Función Usuarios aleatorios:
-     const generarNombreUsuarioAleatorio = () => {
-      const nombre = 'Usuario';
-      const numero = Math.floor(Math.random() * 1000001);
-      
-      return `${nombre}${numero}`;
-    };
-    // Generar y establecer el nombre de usuario aleatorio
-    const nombreUsuarioAleatorio = generarNombreUsuarioAleatorio();
-   
-    // ---------Función Usuario aleatorio------------------
-    //------Función de Fecha de ingreso ------------
-    const obtenerFechaIngreso = () => {
-      const fecha = new Date();
-      const dia = fecha.getDate(); // Asegura que el día tenga 2 dígitos
-      const mes = fecha.getMonth()+1; // Asegura que el mes tenga 2 dígitos
-      const año = fecha.getFullYear();
-      
-      return(`${mes}/${dia}/${año}`);
-    };
-
-    const fechaIngreso = obtenerFechaIngreso();
-    setFechaIngreso(fechaIngreso)
-
-    //------Función de Fecha de ingreso ------------
-    //------Función de hora de ingreso -----------
-    const obtenerHoraIngreso = () => {
-      const fecha = new Date();
-      const opcionesHora = { hour: 'numeric', minute: 'numeric', second: 'numeric' };
-      const horaFormateada = fecha.toLocaleTimeString('es-ES', opcionesHora);
-        
-      return `${horaFormateada}`;
-    };
-
-    const horaIngresoActual = obtenerHoraIngreso();
-    
-//------Función de hora de ingreso -----------
-
-      const obtenerHoraIngresoTemporal = () => {
-        const fecha = new Date();
-        const opcionesHora = { hour: 'numeric', minute: 'numeric', second: 'numeric' };
-
-        // Sumar una hora (3600 segundos) al tiempo actual
-        fecha.setTime(fecha.getTime() - 3600 * 1000);
-
-        const horaIngresoActual = fecha.toLocaleTimeString('es-ES', opcionesHora);
-          
-        return `${horaIngresoActual}`;
-      };
-
-      const horaIngresoActualTemporal = obtenerHoraIngresoTemporal();
-      setHoraIngreso(horaIngresoActualTemporal);
-
-// Actualización de variable de usuario
-    setNombreUsuario(`${nombreUsuarioAleatorio}${fechaIngreso}${horaIngresoActual}`);
-
-    // const userName = `${nombreUsuarioAleatorio}${fechaIngreso}${horaIngresoActual}`;
-// Actualización de variable de usuario
-  
-    //---Función mi Ip -------------
-    async function obtenerMiIp() {
-        try {
-          const response = await fetch('https://api.ipify.org/?format=json');
-          const dataIP = await response.json();
-          setDataIPS(dataIP.ip)
-          return 
-        } catch (error) {
-          console.error(error);
-        }
-      }
-      obtenerMiIp();
-//---Función mi Ip -------------
-
-//---- Función data de mi IP ------
-      async function datosDeMiIP(){
-        try {
-          const response = await fetch(`https://api.geoiplookup.net/?query=${miIP}&json=true`);
-          const datosIP = await response.json();
-          setMiPais(datosIP.countryname ? datosIP.countryname : "Colombia");
-          setMiCiudad(datosIP.city ? datosIP.city : "Ciudad no registrada");
-          // setVarMiIP(datosIP.countryname ? datosIP.countryname : "Colombia");
-        } catch (error) {
-          console.error(error);
-          setMiPais("Colombia")
-          setMiCiudad("Ciudad no registrada");
-        }
-      }
-      datosDeMiIP();
-
-//---- Función data de mi IP ------
-  
-//------Función ruta --------------
-    const ruta = window.location.pathname;
-    setRuta(ruta);
-//------Función ruta --------------
-
-//----Función tiempo de permanencia------
-      const intervaloTiempo = setInterval(() => {
-        const horaActual = new Date();
-        const diferencia = horaActual - tiempoInicio;
-
-        const segundos = Math.floor((diferencia / 1000) % 60);
-        const minutos = Math.floor((diferencia / 1000 / 60) % 60);
-        const horas = Math.floor(diferencia / 1000 / 60 / 60);
-
-        const tiempoFormateado = `${String(horas).padStart(2, '0')}:${String(minutos).padStart(2, '0')}:${String(segundos).padStart(2, '0')}`;
-        setTiempoPermanencia(tiempoFormateado);
-        return tiempoFormateado
-      }, 1000);
-      console.log("Soy tiempo intervalo:", tiempo);
-
-//----Función tiempo de permanencia------
-
-const objetoDataUsuario = {
-  "usuario": usuario,
-  "fecha_ingreso": fechaIngreso,
-  "pais": miPais,
-  "hora_ingreso": horaIngresoActual,
-  "ciudad": miCiudad,
-  "ruta": ruta,
-  "tiempo": tiempo,
-  "dispositivo": dispositivo
-};
-
-console.log("Soy objeto interno:", objetoDataUsuario)
-
-const enviarDatosfunctionCambioPagina = () => {
-  enviarDatos(objetoDataUsuario);
-}
-
-
-   //--------función evento página --------------
-
-   const beforeUnloadHandler = () => {
-    enviarDatosfunctionCambioPagina();
-  }
-  // Agregar el oyente del evento beforeunload
-  window.addEventListener('beforeunload', beforeUnloadHandler);
-
-  //--------función evento página --------------
-
-
-  // Condicional para saber el tipo de dispositivo-------
+useEffect(() => {
     if (window.innerWidth <= 768) {
       setDispositivo('celular');
     }else{
       setDispositivo('computador');
     }
-  // Condicional para saber el tipo de dispositivo-------
+  }, [])
 
-  // función de tiempo aleatorio---------
-  function tiempoAleatorio() {
-    const minutosAleatorios = Math.floor(Math.random() * 11);
-    const segundosAleatorios = Math.floor(Math.random() * 60);
-    const horas = '00';
-    const minutos = minutosAleatorios.toString().padStart(2, '0');
-    const segundos = segundosAleatorios.toString().padStart(2, '0');
-
-    const tiempo = `${horas}:${minutos}:${segundos}`;
-  
-    return tiempo;
+useEffect(() => {
+    async function obtenerMiIp() {
+      try {
+        const response = await fetch('https://api.ipify.org/?format=json')
+        const dataIP = await response.json()
+        setDataIPS(dataIP.ip)
+        return 
+      } catch (error) {
+        console.error(error)
+      }
   }
-  
-  const tiempoRandom = tiempoAleatorio();
-  setTiempoAleatorio(tiempoRandom);
-  // función de tiempo aleatorio---------
+  obtenerMiIp()
+}, [])
 
-  
+useEffect(() => {  
+    async function datosDeMiIP(){
+      try {
+        const response = await fetch(`https://json.geoiplookup.io/${miIP}`)
+        const datosIP = await response.json()
+        setMiPais(datosIP.country_name ? datosIP.country_name : miPaisAleatorio)
+        setMiCiudad(datosIP.district ? datosIP.district : miCiudadAleatorio)
+      } catch (error) {
+        setMiPais(miPaisAleatorio)
+        setMiCiudad(miCiudadAleatorio)
+      }
+    }
+    datosDeMiIP()
+  }, [miIP])
 
-    //--------Resetear----------------
-    return () => {
-      window.removeEventListener('beforeunload', beforeUnloadHandler);
-      clearInterval(intervaloTiempo);
-    };
-   //--------Resetear----------------
-  }, [tiempo]); 
+useEffect(() => {
+  const ruta = window.location.pathname;
+  setRuta(ruta);
+}, [])
 
+useEffect(() => {
+  if (!hasRun.current && 
+    (objetoData.dispositivo === 'computador' || objetoData.dispositivo === 'celular') && 
+    (objetoData.pais !== '' && objetoData.ciudad !== '')) {
+    console.log(objetoData)
+    enviarDatos(objetoData)
+    hasRun.current = true;
+  }
+}, [objetoData])
 
 
   return(
